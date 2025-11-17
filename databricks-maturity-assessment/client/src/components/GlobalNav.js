9f479898d4a1ac617352baad8fb58add68fbff5a
nitinaggarwaldbx <53987770+nitinaggarwaldbx@users.noreply.github.com>
2025-11-17 00:59:41 -0500

    Fix: Replace FiPresentation with FiMonitor icon (FiPresentation doesn't exist in react-icons/fi)

--- databricks-maturity-assessment/client/src/components/GlobalNav.js
+++ databricks-maturity-assessment/client/src/components/GlobalNav.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
-import { FiMenu, FiX, FiPlay, FiList, FiLogIn, FiLogOut, FiUser, FiFileText, FiUsers, FiSend, FiChevronDown, FiLock, FiUserPlus, FiMail, FiMessageSquare, FiSettings, FiBook, FiPresentation } from 'react-icons/fi';
+import { FiMenu, FiX, FiPlay, FiList, FiLogIn, FiLogOut, FiUser, FiFileText, FiUsers, FiSend, FiChevronDown, FiLock, FiUserPlus, FiMail, FiMessageSquare, FiSettings, FiBook, FiMonitor } from 'react-icons/fi';
 import toast from 'react-hot-toast';
 import * as assessmentService from '../services/assessmentService';
 import authService from '../services/authService';
@@ -960,7 +960,7 @@ const GlobalNav = () => {
                       navigate('/pitch-deck');
                       setAdminDropdownOpen(false);
                     }}>
-                      <FiPresentation />
+                      <FiMonitor />
                       Pitch Deck
                     </DropdownItem>
                     
